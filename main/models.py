2f0650945c52671c203607f19589293a9f186cd4
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2016-02-17 18:21:08 +0530

    PollingBooth model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
 # Create your models here.
+
+
+class PollingBooth(models.Model):
+	region_name = models.CharField(max_length=250, unique=True)
+	number = models.CharField(max_length=10, unique=True)
+	latitude = models.DecimalField(max_digits=3,  decimal_places=10)
+	longitude = models.DecimalField(max_digits=3,  decimal_places=10)
+
+	def __str__(self):
+		return self.region_name
+
