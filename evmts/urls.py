cf3a31b07a663b83a2c4c280eaa9fbe95fa41fa8
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2016-02-18 21:13:42 +0530

    By Aklank

--- evmts/urls.py
+++ evmts/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from main.views import *
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^recieved_evm/', RecievedEvm.as_view() , name = 'recieved_evm'),
+    url(r'^reached_ps/' , ReachedPS.as_view() , name = 'reached_ps'),
+    url(r'^polling_cond/', PollingCond.as_view() , name = 'polling_cond')
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.generic import View
+from django.http import JsonResponse
 # Create your views here.
+
+class RecievedEvm(View):
+
+	def post(self, request):
+
+		return JsonResponse({'result' : 'ok' })
+	def get(self , request):
+		return JsonResponse({'result' : 'ok'})
+
+
+class ReachedPS(View):
+
+	def post(self , request):
+
+		return JsonResponse({'result' : 'ok'})
+
+class PollingCond(View):
+
+	def post(self , request):
+
+		if cond == 'good':
+			return JsonResponse({'result' : 'good'})
+		else:
+			return JsonResponse({'result' : 'ok'})
