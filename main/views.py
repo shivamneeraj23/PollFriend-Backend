19982d7179912d5c65ee2a79674a5ec3319de685
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2016-02-18 21:07:13 +0530

    Login of Presiding Officer with Polling Station

--- main/views.py
+++ main/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.views.generic import View
+from main.models import PresidingOfficer, PollingStation
+from datetime import datetime
+import hashlib
 # Create your views here.
+
+
+class LoginPO(View):
+
+	def post(self, request):
+		poid = request.POST.get('poid')
+		psid = request.POST.get('psid')
+		try:
+			ps = PollingStation.objects.get(unique_id=psid)
+			presiding_officer = PresidingOfficer.objects.get(polling_station=ps, username=poid)
+			password = (psid+'@#'+poid+str(datetime.date(datetime.now()))).encode('utf-8')
+			key = hashlib.sha256(password)
+			presiding_officer.api_key = key.hexdigest()
+			presiding_officer.save()
+			return JsonResponse({'result': 'ok', 'access_token': presiding_officer.api_key})
+		except PresidingOfficer.DoesNotExist:
+			return JsonResponse({'result': 'fail'})
+		except PollingStation.DoesNotExist:
+			return JsonResponse({'result': 'fail'})
+
+
+
