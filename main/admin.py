0a255aefcc619557c4038e4bf4d4611e48cbe50b
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2016-02-18 14:49:42 +0530

    Registered models on Admin site

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from main.models import *
+
+admin.site.register(LAC)
+admin.site.register(PollingStation)
+admin.site.register(PresidingOfficer)
+admin.site.register(PollUpdate)
+admin.site.register(EmergencyContact)
+admin.site.register(POStatus)
+admin.site.register(SOSUpdate)
+
+
