b290c288de92e3623648dd390894a0523e5e6c71
Aklank Jain <aj.aklank@live.com>
2016-02-22 02:38:11 +0530

    If polling station total voters not provided then 0%

--- main/templatetags/main_extras.py
+++ main/templatetags/main_extras.py
@@ -8,7 +8,7 @@ register = template.Library()
 def current_vote_percentage(polling_station):
 	pu = PollUpdate.objects.order_by('-timestamp').filter(polling_station=polling_station)
 	percentage = 0
-	if len(pu) > 0:
+	if len(pu) > 0 and polling_station.total_voters:
 		pu = pu[0]
 		percentage = (pu.current_votes * 100)/polling_station.total_voters
 	return percentage
