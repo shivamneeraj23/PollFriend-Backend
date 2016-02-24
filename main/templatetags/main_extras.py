358681df3487525fd869924ef5a3a0b761cfb753
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2016-02-24 16:07:30 +0530

    Detailed tool tip map locations.

--- main/templatetags/main_extras.py
+++ main/templatetags/main_extras.py
@@ -15,6 +15,16 @@ def current_vote_percentage(polling_station):
 
 
 @register.simple_tag
+def current_voters(polling_station):
+	pu = PollUpdate.objects.order_by('-timestamp').filter(polling_station=polling_station)
+	current_votes = 0
+	if len(pu) > 0 and polling_station.total_voters:
+		pu = pu[0]
+		current_votes = pu.current_votes
+	return current_votes
+
+
+@register.simple_tag
 def get_current_location(presiding_officer):
 	po_location = POLocation.objects.order_by('-timestamp').filter(presiding_officer=presiding_officer)
 	lat, long = None, None
