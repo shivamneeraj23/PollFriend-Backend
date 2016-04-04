381d46f1d5e2d8de9d3fec02288f3ed438ac51b2
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2016-04-04 08:33:33 +0530

    POStatus Admin Panel

--- main/admin.py
+++ main/admin.py
@@ -19,12 +19,20 @@ class PollUpdateAdmin(admin.ModelAdmin):
 	list_display = ['polling_station', 'current_votes', 'time_field', 'timestamp']
 	search_fields = ('polling_station__unique_id', 'polling_station__name', 'time_field', 'current_votes')
 
+
+class POStatusAdmin(admin.ModelAdmin):
+	list_display = ['presiding_officer', 'mock_poll_starts', 'mock_poll_resetted', 'mock_poll_ends', 'poll_starts',
+	                'poll_ends']
+	search_fields = ('presiding_officer__polling_station__unique_id', 'presiding_officer__polling_station__name',
+	                 'presiding_officer__first_name')
+
+
 admin.site.register(LAC)
 admin.site.register(PollingStation, PollingStationAdmin)
 admin.site.register(PresidingOfficer, PresidingOfficerAdmin)
 admin.site.register(PollUpdate, PollUpdateAdmin)
 admin.site.register(SectorOffice)
-admin.site.register(POStatus)
+admin.site.register(POStatus, POStatusAdmin)
 # admin.site.register(SOSUpdate)
 # admin.site.register(EVM)
 # admin.site.register(POLocation)
@@ -32,4 +40,3 @@ admin.site.register(POStatus)
 # admin.site.register(WebDevice)
 # admin.site.register(Message)
 admin.site.register(OtherDetails)
-
